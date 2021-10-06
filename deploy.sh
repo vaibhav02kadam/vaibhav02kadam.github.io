294dd17a1e77e81f7955567d60269bc8cdbc0cc6
Vaibhav <vaibhav02kadam@gmail.com>
2021-10-06 22:23:02 +0530

    added deploy

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,5 @@ echo "Generating site"
 hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
